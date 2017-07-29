60cf2093e5e3acedd3a3adec8c4869f846625879
varunagarwal2008 <varun.agrwl96@gmail.com>
2017-07-30 03:53:41 +0530

    modified

--- php_processing/order_listing.php
+++ php_processing/order_listing.php
@@ -7,7 +7,7 @@ if(isset($_GET['sakhi_phone'])){
   $query = mysqli_query($connection, $sql);
   $row = mysqli_fetch_row($query);
   $sakhi_id = $row[0];
-  $sql = "SELECT *  FROM orders WHERE sakhi_id = $sakhi_id AND status = 0";
+  $sql = "SELECT *  FROM orders WHERE sakhi_id = $sakhi_id AND status = 1";
   $query = mysqli_query($connection, $sql);
   $i=0;
   $arr = array();
@@ -15,7 +15,7 @@ if(isset($_GET['sakhi_phone'])){
 	
   if (mysqli_num_rows($query) > 0)
   {
-    while($row = mysqli_fetch_assoc($query))
+    while($row = mysqli_fetch_array($query))
     {
 		$r[$i]=$row;
 		//$arr=['id'=>$row['id'], 'customer_id'=>$row['customer_id'],'quantity'=>$row['inventory_id'],'timestamp'=>$row['orderTS'],'address'=>$row['delivery_address'],'price'=>$row['price']];
