05c7397a8f08d2fa774b5ad43e858aab9087f032
Tanmay Rauth <tanmayrauth123@gmail.com>
2017-07-30 02:02:39 +0530

    28

--- php_processing/order_listing.php
+++ php_processing/order_listing.php
@@ -1,5 +1,7 @@
 <?php
 include_once('php_includes/db_conx.php');
+
+if(isset($_GET['sakhi_']))
 $sql = "SELECT *  FROM orders WHERE status = 0";
 $query = mysqli_query($connection, $sql);
 echo $sql;
