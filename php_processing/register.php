456b81f1c9f87c5756071f5194be995b75de72c3
Tanmay Rauth <tanmayrauth123@gmail.com>
2017-07-29 16:24:15 +0530

    3 commit

--- php_processing/register.php
+++ php_processing/register.php
@@ -1,85 +1,42 @@
 <?php include_once("php_includes/check_login_status.php");?>
 <?php
-// Ajax calls this NAME CHECK code to execute
-if(isset($_POST["mobile_no"])){
-	include_once("php_includes/db_conx.php");
-	$mobile_no = preg_replace('#[^a-z0-9]#i', '', $_POST['mobile_no']);
-	$sql = "SELECT id FROM users WHERE mobile_no='$mobile_no' LIMIT 1";
-    $query = mysqli_query($connection, $sql);
-    $uname_check = mysqli_num_rows($query);
 
-    if ($uname_check < 1) {
-	    echo '';
-	    exit();
-    } else {
-	    echo 'already_registered';
-	    exit();
-    }
-}
-?><?php
-// Ajax calls this REGISTRATION code to execute
-if(isset($_POST["mobile_no"])){
+if(isset($_GET["mobile_no"])){
 	// CONNECT TO THE DATABASE
 	include_once("php_includes/db_conx.php");
-	// GATHER THE POSTED DATA INTO LOCAL VARIABLES
-	$mobile_no = preg_replace('#[^0-9]#', '', $_POST['mobile_no']);
-	$p = $_POST['password'];
-	$user_type = preg_replace('#[^a-z ]#i', '', $_POST['user_type']);
-	// GET USER IP ADDRESS
-    $ip = preg_replace('#[^0-9.]#', '', getenv('REMOTE_ADDR'));
-	// DUPLICATE DATA CHECKS FOR USERNAME AND EMAIL
-	$sql = "SELECT id FROM users WHERE username='$u' LIMIT 1";
-    $query = mysqli_query($connection, $sql);
+	// GATHER THE GETED DATA INTO LOCAL VARIABLES
+	$mobile_no = preg_replace('#[^0-9]#', '', $_GET['mobile_no']);
+	$p = $_GET['password'];
+	$user_type = preg_replace('#[^0-2]#i', '', $_GET['user_type']);
+	$sql = "SELECT id FROM users WHERE mobile_no='$mobile_no' LIMIT 1";
+  $query = mysqli_query($connection, $sql);
 	$u_check = mysqli_num_rows($query);
-	// -------------------------------------------
-	$sql = "SELECT id FROM users WHERE email='$e' LIMIT 1";
-    $query = mysqli_query($connection, $sql);
-	$e_check = mysqli_num_rows($query);
-	// FORM DATA ERROR HANDLING
-	if($u == "" || $e == "" || $p == "" || $g == "" || $c == ""){
-		echo "The form submission is missing values.";
-        exit();
+	if($p == "" || $user_type == "" || $mobile_no == ""){
+		echo "missing_values";
+    exit();
 	} else if ($u_check > 0){
-        echo "The username you entered is alreay taken";
-        exit();
-	} else if ($e_check > 0){
-        echo "That email address is already in use in the system";
-        exit();
-	} else if (strlen($u) < 3 || strlen($u) > 16) {
-        echo "Username must be between 3 and 16 characters";
-        exit();
-    } else if (is_numeric($u[0])) {
-        echo 'Username cannot begin with a number';
-        exit();
-    } else {
-	// END FORM DATA ERROR HANDLING
-	    // Begin Insertion of data into the database
-		// Hash the password and apply your own mysterious unique salt
-		$p_hash=md5($p);
-		// Add user info into the database table for the main site table
-		$sql = "INSERT INTO users (username, email, password, gender, country, ip, signup, lastlogin, notescheck)
-		        VALUES('$u','$e','$p_hash','$g','$c','$ip',now(),now(),now())";
+    echo "already_registered";
+    exit();
+	}else {
+		$p_hash = md5($p);
+		$name= $_GET['name'];
+    $sql = "INSERT INTO users (mobile_no, password, user_type, reg_date)
+		        VALUES('$mobile_no','$p_hash','$user_type',now())";
 		$query = mysqli_query($connection, $sql);
 		$uid = mysqli_insert_id($connection);
-		// Establish their row in the useroptions table
-		$sql = "INSERT INTO useroptions (id, username, background) VALUES ('$uid','$u','original')";
-		$query = mysqli_query($connection, $sql);
-		// Create directory(folder) to hold each user's files(pics, MP3s, etc.)
-		if (!file_exists("user/$u")) {
-			mkdir("user/$u", 0755);
-		}
-		// Email the user their activation link
-		$to = "$e";
-		$from = "response@trailhub.bugs3.com";
-		$subject = 'trialhub Account Activation';
-		$message = '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>trialhub Message</title></head><body style="margin:0px; font-family:Tahoma, Geneva, sans-serif;"><div style="padding:10px; background:#333; font-size:24px; color:#CCC;"><a href="http://www.yoursitename.com"><img src="http://www.yoursitename.com/images/logo.png" width="36" height="30" alt="yoursitename" style="border:none; float:left;"></a>yoursitename Account Activation</div><div style="padding:24px; font-size:17px;">Hello '.$u.',<br /><br />Click the link below to activate your account when ready:<br /><br /><a href="http://www.yoursitename.com/activation.php?id='.$uid.'&u='.$u.'&e='.$e.'&p='.$p_hash.'">Click here to activate your account now</a><br /><br />Login after successful activation using your:<br />* E-mail Address: <b>'.$e.'</b></div></body></html>';
-		$headers = "From: $from\n";
-        $headers .= "MIME-Version: 1.0\n";
-        $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-		//mail($to, $subject, $message, $headers);
-		echo "signup_success";
-		exit();
-	}
+
+    if($user_type == 1 ){
+      $availablity = 1;
+  		$sql = "INSERT INTO sakhis (id, name, availablity) VALUES ('$uid','$name','$availablity')";
+  		$query = mysqli_query($connection, $sql);
+  	}
+    if($user_type == 2 ){
+      $availablity = 1;
+      $lat = $_GET['lat'];
+      $long = $_GET['long'];
+  		$sql = "INSERT INTO customers (id, name, availablity, lat, long) VALUES ('$uid','$name','$availablity','$lat', '$long')";
+  		$query = mysqli_query($connection, $sql);
+  	}
 	exit();
 }
 ?>
