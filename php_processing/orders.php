da4713264ad2da9ab8e8fbc1caa0c1d9a88af108
Tanmay Rauth <tanmayrauth123@gmail.com>
2017-07-29 22:47:49 +0530

    18

--- php_processing/orders.php
+++ php_processing/orders.php
@@ -9,26 +9,26 @@ if(isset($_GET["cust_phone"])){
 	$cust_id = $row[0];
 	$quantity = preg_replace('#[^0-9]#', '', $_GET['quantity']);
 	$inv_id = preg_replace('#[^0-9]#i', '', $_GET['inv_id']);
-	$price = preg_replace('#[^0-9]#', '', $_GET['price']);
+	$price =  $inv_id * 10;             //preg_replace('#[^0-9]#', '', $_GET['price']);
 	$sakhi_id = $_GET['sakhi_id'];
 	$address = "";
 
-	if($_GET['flag'] == 1)
+	if(isset($_GET['address']) )
 		$address = mysqli_escape_string($_GET['address']);
-	else{
+	/*
 		$sql = "SELECT address  FROM customers WHERE mobile_no=$m LIMIT 1";
 	  $query = mysqli_query($connection, $sql);
 		echo $sql;
 	  $row = mysqli_fetch_row($query);
 		$address = $row[0];
-	}
+		*/
 
 	if($quantity == "" || $cust_id == "" || $price == "" || $inv_id == ""){
 		echo "missing_values";
     exit();
 	}
 	else {
-    $sql = "INSERT INTO order (cust_id, quantity, inv_id, orderTS, sakhi_id,  address, price);
+    $sql = "INSERT INTO order (customer_id, quantity, inventory_id, orderTS, sakhi_id,  delivery_address, price);
 		        VALUES($cust_id, $quantity, $inv_id, now(), $sakhi_id, $address, $price)";
     echo $sql;
     $query = mysqli_query($connection, $sql);
