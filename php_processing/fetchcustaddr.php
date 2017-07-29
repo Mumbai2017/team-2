08ae8bc62a020095db123b18f485f46fa9159cb5
varunagarwal2008 <varun.agrwl96@gmail.com>
2017-07-30 02:14:17 +0530

    File

--- php_processing/fetchcustaddr.php
+++ php_processing/fetchcustaddr.php
@@ -3,7 +3,7 @@
 if(isset($_GET["mobile_no"])){
 	// CONNECT TO THE DATABASE
 	$m = $_GET["mobile_no"];
-	$sql = "SELECT id  FROM users WHERE mobile_no='$m' LIMIT 1";
+	$sql = "SELECT id  FROM users WHERE mobile_no=$m LIMIT 1";
 	$query = mysqli_query($connection, $sql);
 	$row = mysqli_fetch_row($query);
 	$cust_id = $row[0];
@@ -13,11 +13,11 @@ if(isset($_GET["mobile_no"])){
 	
 	// END FORM DATA ERROR HANDLING
 	
-  	$sql = "SELECT address FROM customers WHERE id='$cust_id'";
+  	$sql = "SELECT address FROM customers WHERE id=$cust_id";
 	echo $sql
   	$query = mysqli_query($connection, $sql);
         $row = mysqli_fetch_row($query);
-			echo $row[0];
+			echo $row[2];
 		}
 	
 ?>
