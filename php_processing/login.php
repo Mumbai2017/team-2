ea0fd4eb2cfcd26ad48ef0ccfb6a2c799cd66b11
Tanmay Rauth <tanmayrauth123@gmail.com>
2017-07-29 15:21:54 +0530

    first commit

---
+++ php_processing/login.php
@@ -0,0 +1,49 @@
+
+
+
+<?php
+include_once("php_includes/check_login_status.php");
+// If user is already logged in, header that weenis away
+?>
+<?php
+
+if(isset($_POST["email"])){
+	// CONNECT TO THE DATABASE
+	include_once("php_includes/db_conx.php");
+	// GATHER THE POSTED DATA INTO LOCAL VARIABLES AND SANITIZE
+	$e = mysqli_real_escape_string($connection, $_POST['email']);
+	$p = md5($_POST['password']);
+	if($e == "" || $p == ""){
+		echo "login_failed";
+    exit();
+	}
+  else {
+	// END FORM DATA ERROR HANDLING
+		$sql = "SELECT id, username, password FROM users WHERE email='$e' AND activated='1' LIMIT 1";
+        $query = mysqli_query($connection, $sql);
+        $row = mysqli_fetch_row($query);
+		$db_id = $row[0];
+		$db_username = $row[1];
+        $db_pass_str = $row[2];
+		if($p != $db_pass_str){
+			echo "login_failed";
+            exit();
+		} else {
+			// CREATE THEIR SESSIONS AND COOKIES
+			$_SESSION['userid'] = $db_id;
+			$_SESSION['username'] = $db_username;
+			$_SESSION['password'] = $db_pass_str;
+			setcookie("id", $db_id, strtotime( '+30 days' ), "/", "", "", TRUE);
+			setcookie("user", $db_username, strtotime( '+30 days' ), "/", "", "", TRUE);
+    		setcookie("pass", $db_pass_str, strtotime( '+30 days' ), "/", "", "", TRUE);
+			// UPDATE THEIR "IP" AND "LASTLOGIN" FIELDS
+			$sql = "UPDATE users SET ip='$ip', lastlogin=now() WHERE username='$db_username' LIMIT 1";
+            $query = mysqli_query($connection, $sql);
+			echo $db_username;
+		    exit();
+		}
+	}
+	exit();
+}
+?>
+php
