53164ff6b48f55dbc0d3492efa3aea8f072cae78
varunagarwal2008 <varun.agrwl96@gmail.com>
2017-07-29 17:02:19 +0530

    Cust Register and GPS

--- ChaakriApp/app/src/main/java/com/cfg/chaakri/chaakri/CustomerRegister.java
+++ ChaakriApp/app/src/main/java/com/cfg/chaakri/chaakri/CustomerRegister.java
@@ -2,12 +2,34 @@ package com.cfg.chaakri.chaakri;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CustomerRegister extends AppCompatActivity {
-
+    GPSTracker gps;
+EditText username,mobilenumber;
+    Button registerbutton;
+    double latitude, longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_register);
+        username=(EditText)findViewById(R.id.username);
+        mobilenumber=(EditText)findViewById(R.id.mobilenumber);
+        registerbutton=(Button)findViewById(R.id.registerbutton);
+        registerbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gps = new GPSTracker(CustomerRegister.this);
+                if (gps.canGetLocation()) {
+                    latitude = gps.latitude;
+                    longitude = gps.longitude;
+                }
+                String user=username.getText().toString();
+                String number=mobilenumber.getText().toString();
+                String message= user+","+number+",2,"+latitude+","+longitude;
+            }
+        });
     }
 }
