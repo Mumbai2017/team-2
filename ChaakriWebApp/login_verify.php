4d66d27f8c86e48d0a04cb5eab432882f4d4250b
v-sarvaswa <venkzz@sarvaswa.com>
2017-07-30 01:59:40 +0530

    NGO Login [R1]

---
+++ ChaakriWebApp/login_verify.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST['form3']) && $_POST['form4'])
+{
+	$username = $_POST['form3'];
+	$password = $_POST['form4'];
+	if($username == "admin" && $password == "admin")
+	{
+		
+	}
+	else
+	{
+		
+	}
+}
+?>
