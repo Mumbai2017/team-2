33843dc0e1ebc1dfef90f6c333e00c88d1e78dcc
varunagarwal2008 <varun.agrwl96@gmail.com>
2017-07-29 15:50:16 +0530

    Check intent

--- ChaakriApp/app/src/main/java/com/cfg/chaakri/chaakri/MainActivity.java
+++ ChaakriApp/app/src/main/java/com/cfg/chaakri/chaakri/MainActivity.java
@@ -1,13 +1,33 @@
 package com.cfg.chaakri.chaakri;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button customerloginbutton,sakhiloginbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        customerloginbutton=(Button)findViewById(R.id.customerloginbutton);
+        sakhiloginbutton=(Button)findViewById(R.id.sakhiloginbutton);
+        customerloginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+
+            }
+        });
+        sakhiloginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),SakhiFirst.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
