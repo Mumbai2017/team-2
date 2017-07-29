8645bd63a95b2455abc004b9c44e2b6fd1c0713e
v-sarvaswa <venkzz@sarvaswa.com>
2017-07-29 20:52:38 +0530

    NGO Orders R3

--- php_processing/ngo_orders.php
+++ php_processing/ngo_orders.php
@@ -4,6 +4,7 @@ include_once("php_includes/db_conx.php");
 if(isset($_GET["sakhi_phone"])){
 	// CONNECT TO THE DATABASE
 	// GATHER THE GETED DATA INTO LOCAL VARIABLES
+	//Push
 	$m=$_GET["sakhi_phone"];
 	$sql = "SELECT id  FROM users WHERE mobile_no=$m LIMIT 1";
   $query = mysqli_query($connection, $sql);
