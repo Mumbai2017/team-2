6948f0c742aeaf9f7393d324a1cda2457bb20a15
Dhruva Gaidhani <dhruvagaidhani@hotmail.com>
2017-07-30 01:47:41 +0530

    php files address

--- php_processing/fetchcustaddr.php
+++ php_processing/fetchcustaddr.php
@@ -1,20 +1,28 @@
-<?php
+<?php include_once("php_includes/db_conx.php");
+
 if(isset($_GET["mobile_no"])){
 	// CONNECT TO THE DATABASE
-	include_once("php_includes/db_conx.php");
-	// GATHER THE POSTED DATA INTO LOCAL VARIABLES AND SANITIZE
-  $id = $_GET['id'];
 	$m = $_GET["mobile_no"];
+	$sql = "SELECT id  FROM users WHERE mobile_no=$m LIMIT 1";
+	$query = mysqli_query($connection, $sql);
+	echo $sql;
+	$row = mysqli_fetch_row($query);
+	$cust_id = $row[0];
+
+	// GATHER THE POSTED DATA INTO LOCAL VARIABLES AND SANITIZE
+ 	$id = $_GET['id'];
 	if($m == ""){
 		echo "login_failed";
     exit();
 	}
-  else {
+ 	else {
 	// END FORM DATA ERROR HANDLING
-	$sql = "SELECT address FROM customers WHERE id=$id;
-  $query = mysqli_query($connection, $sql);
+	
+  	$sql = "SELECT address FROM customers WHERE id=$cust_id";
+	echo $sql
+  	$query = mysqli_query($connection, $sql);
         $row = mysqli_fetch_row($query);
 			echo $row;
-		
+		}
 	}
 ?>
