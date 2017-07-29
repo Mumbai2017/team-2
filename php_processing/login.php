0afea360c504f08bb2c1135fb28873829cfa1372
v-sarvaswa <venkzz@sarvaswa.com>
2017-07-29 17:40:04 +0530

    Edit Login 3

--- php_processing/login.php
+++ php_processing/login.php
@@ -5,14 +5,14 @@ if(isset($_GET["mobile_no"])){
 	// GATHER THE POSTED DATA INTO LOCAL VARIABLES AND SANITIZE
   $password = $_GET['password'];
 	$p = md5($password);
-	$m = $_GET["mobile_no"]);
+	$m = $_GET["mobile_no"];
 	if($m == "" || $p == ""){
 		echo "login_failed";
     exit();
 	}
   else {
 	// END FORM DATA ERROR HANDLING
-	$sql = "SELECT id, password FROM users WHERE phone_no=$phone_no LIMIT 1";
+	$sql = "SELECT id, password FROM users WHERE mobile_no=$m LIMIT 1";
   $query = mysqli_query($connection, $sql);
         $row = mysqli_fetch_row($query);
 		$db_id = $row[0];
